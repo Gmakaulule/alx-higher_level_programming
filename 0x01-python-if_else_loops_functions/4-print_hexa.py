0d3cd7027294483cdc8dcf366fc151e6ba1f75b0
Gmakaulule <giftmakaulule@gmail.com>
2022-08-23 21:40:16 +0000

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 for i in range(0, 99):
     if i != 101 and i != 113:
-        print(f'{i} = {hex(i)}', end="\n")
+        print('{} = {}'.format(i, hex(i)), end="\n")
