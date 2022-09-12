dd0fd7dbf47e1b69aa5f9a145dd4f62251e37b01
Gmakaulule <giftmakaulule@gmail.com>
2022-09-12 18:44:02 +0000

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+from curses.ascii import isdigit
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError,ValueError):
+        return False
