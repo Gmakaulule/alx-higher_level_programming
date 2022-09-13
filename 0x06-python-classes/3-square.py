f96a746d6c600c8cee1f21a87d413b04f84433e8
Gmakaulule <giftmakaulule@gmail.com>
2022-09-13 19:02:05 +0000

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Definition of squre class"""
+
+
+class Square:
+    """This is the squre class
+    Atributes:
+    _size (int): length of squre
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        try:
+            self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except size < 0:
+                raise ValueError("size must be >= 0")
+
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
