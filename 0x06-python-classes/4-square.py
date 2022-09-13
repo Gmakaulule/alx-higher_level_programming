57cd5bff01fea7d2c5224609875785096112819d
Gmakaulule <giftmakaulule@gmail.com>
2022-09-13 19:07:29 +0000

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Definition of squre class"""
+
+
+class Square:
+    """This is the squre class
+    Atributes:
+    _size (int): length of squre
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        try:
+            self.__size = size
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except size < 0:
+                raise ValueError("size must be >= 0")
+
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
+    
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): the size of a size of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
