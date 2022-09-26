489ac686c739fa5baeb885b1f61985945907fb14
Gmakaulule <giftmakaulule@gmail.com>
2022-09-26 19:57:29 +0000

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
+"""
+This module returns all atributes and methods of a class
+"""
 def lookup(obj):
+    """ return list of methods and atributes
+    """
     return dir(obj)
