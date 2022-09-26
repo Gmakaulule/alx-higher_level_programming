142a7f92ed1857c46302688187481c99152a01eb
Gmakaulule <giftmakaulule@gmail.com>
2022-09-26 20:07:46 +0000

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
+"""
+check if is instance of a class
+"""
+
+
 def inherits_from(obj, a_class):
+    """check if obj is instance of a_class"""
+
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
