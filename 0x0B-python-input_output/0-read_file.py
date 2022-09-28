57e3047ba90173c8a0cc896ad515bd5d88681e15
Gmakaulule <giftmakaulule@gmail.com>
2022-09-28 11:29:25 +0000

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+"""thi module read files"""
+
 
 def read_file(filename=""):
+    """file reader module"""
+
     with open(filename, encoding='utf-8') as f:
         for line in f:
-            print(line,end="")
+            print(line, end="")
