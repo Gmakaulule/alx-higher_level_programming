ae5f00d47d8137d1139df9f835d4725c337fa5c6
Gmakaulule <giftmakaulule@gmail.com>
2022-09-28 19:00:29 +0000

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''return Json representation of an object'''
+import json
+
+def to_json_string(my_obj):
+    '''serialise python object to JSON string'''
+    return json.dumps(my_obj)
