c8e37f442aa51718614f6355e214d27b70187768
Gmakaulule <giftmakaulule@gmail.com>
2022-09-30 12:52:52 +0000

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A model that contains a Base class to manage
+the id attribute of all classes that extend
+from Base and avoid duplicate the same code.
+"""
+
+class Base:
+    """
+    ...
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        ...
+        """
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
