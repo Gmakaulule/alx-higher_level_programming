80872ea15b311a4b8e89075b0077ed0102933780
Gmakaulule <giftmakaulule@gmail.com>
2022-10-03 18:25:47 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,72 +1,96 @@
 #!/usr/bin/python3
+"""
+...
+"""
 
-'''Importing Base class from file base.py'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class with all properties and methods"""
+    """
+    ...
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        ...
+        """
         super().__init__(id)
-        
+
         self.check_integer_parameter(width, 'width')
         self.check_integer_parameter(height, 'height')
         self.check_integer_parameter(x, 'x')
         self.check_integer_parameter(y, 'y')
-    
+
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        self.__id = id
 
     @property
-    def width_getter(self):
-        '''get the properties of width'''
+    def width(self):
+        """
+        ...
+        """
         return self.__width
 
-    def width_setter(self, width):
-        '''set properties of width'''
-        try:
-            if type(width) == int:
-                self.__width = width
-        except:
-            raise ValueError ('width must be an integer')
+    @width.setter
+    def width(self, param):
+        """
+        ...
+        """
+        self.check_integer_parameter(param, 'width')
+
+        self.__width = param
+
     @property
-    def height_getter(self):
-        '''get properties of height'''
+    def height(self):
+        """
+        ...
+        """
         return self.__height
 
-    def height_setter(self, height):
-        '''set properties of height'''
-        try:
-            if type(height) == int:
-                self.__height = height
-        except:
-            raise ValueError ("height must be an integer")
-    def x_getter(self):
-        '''get properties of x'''
+    @height.setter
+    def height(self, param):
+        """
+        ...
+        """
+        self.check_integer_parameter(param, 'height')
+
+        self.__height = param
+
+    @property
+    def x(self):
+        """
+        ...
+        """
         return self.__x
 
-    def x_setter(self, x):
-        """set properties of x"""
-        try:
-            if type(x) is int:
-                self.__x = x
-        except:
-            raise ValueError ("x must be an integer")
+    @x.setter
+    def x(self, param):
+        """
+        ...
+        """
+        self.check_integer_parameter(param, 'x')
 
-    def y_getter(self):
-        '''get properties of y'''
+        self.__x = param
+
+    @property
+    def y(self):
+        """
+        ...
+        """
         return self.__y
 
-    def y_setter(self, y):
-        '''set properties of y'''
+    @y.setter
+    def y(self, param):
+        """
+        ...
+        """
+        self.check_integer_parameter(param, 'y')
 
-        raise ValueError ("y must be an integer")
-    
-    
+        self.__y = param
+
     def check_integer_parameter(self, value, param):
         """
         ...
@@ -79,3 +103,61 @@ class Rectangle(Base):
 
         if value < 0 and param in ('x', 'y'):
             raise ValueError(param + ' must be >= 0')
+
+    def area(self):
+        """
+        ...
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        ...
+        """
+        if self.__y > 0:
+            print('\n' * self.__y, end='')
+
+        for i in range(self.height):
+            if self.__x > 0:
+                print(' ' * self.__x, end='')
+
+            print('#' * self.__width)
+
+    def __str__(self):
+        """
+        ...
+        """
+        return '[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}'.format(
+            self.id, self.x, self.y, self.width, self.height
+        )
+
+    def update(self, *args, **kwargs):
+        """
+        ...
+        """
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'width', 'height', 'x', 'y']
+
+        if argc > 5:
+            argc = 5
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
+
+    def to_dictionary(self):
+        """
+        ...
+        """
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
